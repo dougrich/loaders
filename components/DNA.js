4f20c5e7bc318cad9368b6b7c32dad24dde2e54a
dougrich <dougrich@users.noreply.github.com>
2021-04-27 12:11:38 -0700

    Added DNA loader

---
+++ components/DNA.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './DNA.css'
+
+export default function DNA () {
+  return (
+    <svg viewBox='-50 -50 100 100' width='100%' height='100%'>
+      <g className='dna__bar dna__bar__0'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__1'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__2'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__3'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__4'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__5'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__6'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__7'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__8'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__9'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__a'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__b'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__c'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__d'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__e'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+      <g className='dna__bar dna__bar__f'>
+        <circle className='dna__m dna__ml' cx='0' cy='0' r='2' />
+        <line className='dna__l' x1='-20' x2='20' y1='0' y2='0' stroke='black' strokeWidth='1' />
+        <circle className='dna__m dna__mr' cx='0' cy='0' r='2' />
+      </g>
+    </svg>
+  )
+}
