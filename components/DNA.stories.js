bd0810a28e0d18c4eb15fb720eab138440f44062
dougrich <dougrich@users.noreply.github.com>
2021-04-27 12:21:42 -0700

    Fix naming issue

--- components/DNA.stories.js
+++ components/DNA.stories.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import DNA from './DNA'
+import DnaLoader from './DNA'
 
 export default {
   title: 'Loader/DNA',
-  component: DNA
+  component: DnaLoader
 }
 
 export const Example = (args) => (
   <div style={{ width: 200, height: 200, margin: 'auto', marginTop: '2em' }}>
-    <DNA {...args} />
+    <DnaLoader {...args} />
   </div>
 )
