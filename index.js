67290625209dcb117b722f7f82726aa0bdd080b3
dougrich <dougrich@users.noreply.github.com>
2021-04-27 13:08:45 -0700

    Incremented version, added loaders to the package

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+import AtomicOrbit from './components/AtomicOrbit'
+import BookSpin from './components/BookSpin'
+import Bouncer from './components/Bouncer'
 import BouncingBall from './components/BouncingBall'
+import BoxLoader from './components/BoxLoader'
+import CircleC from './components/CircleC'
+import ConcentricRings from './components/ConcentricRings'
+import CubeSpin from './components/Cubespin'
+import DNALoader from './components/DNA'
+import DotBounce from './components/DotBounce'
+import DotSpin from './components/DotSpin'
+import DPulse from './components/Dpulse'
+import Dwave from './components/Dwave'
+import FlashSpinner from './components/FlashSpinner'
+import Flywheel from './components/Flywheel'
+import Hourglass from './components/Hourglass'
+import InfiniteLooper from './components/InfiniteLooper'
+import Pulse from './components/Pulse'
+import Rosary from './components/Rosary'
+import Spring from './components/Spring'
 
 export {
-  BouncingBall
+  AtomicOrbit,
+  BookSpin,
+  Bouncer,
+  BouncingBall,
+  BoxLoader,
+  CircleC,
+  ConcentricRings,
+  CubeSpin,
+  DNALoader,
+  DotBounce,
+  DotSpin,
+  DPulse,
+  Dwave,
+  FlashSpinner,
+  Flywheel,
+  Hourglass,
+  InfiniteLooper,
+  Pulse,
+  Rosary,
+  Spring
 }
